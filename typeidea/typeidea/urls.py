4844c0b23801faa3f565acf5d246bc7fcbf200e1
qmy <275394982@qq.com>
2019-04-29 23:45:58 +0800

    抽象基础模板base

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -19,13 +19,13 @@ from typeidea.custom_site import custom_site
 from config.views import links
 from blog.views import post_list,post_detail
 urlpatterns = [
-    url(r'^super_admin/',admin.site.urls),
-    url(r'^admin/', custom_site.urls),
+    url(r'^super_admin/',admin.site.urls,name='super=admin'),
+    url(r'^admin/', custom_site.urls,name='admin'),
 ]
 urlpatterns +=[
-    url(r'^$',post_list),
-    url(r'^category/(?P<category_id>\d+)/$',post_list),
-    url(r'^tag/(?P<tag_id>\d+)/$',post_list),
-    url(r'^post/(?P<post_id>\d+).html$',post_detail),
-    url(r'^links/$',links)
+    url(r'^$',post_list,name='index'),
+    url(r'^category/(?P<category_id>\d+)/$',post_list,name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$',post_list,name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html$',post_detail,name='post-detail'),
+    url(r'^links/$',links,name='links')
 ]
